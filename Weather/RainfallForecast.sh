8a976118ffa974f444bb758125bf3720db3d0c30
vasudevantv <vasudevantv@yahoo.co.in>
2023-06-10 12:09:20 +0530

    Add files via upload

---
+++ Weather/RainfallForecast.sh
@@ -0,0 +1,2 @@
+rm district_rainfall_forecast.pdf
+wget https://mausam.imd.gov.in/thiruvananthapuram/mcdata/district_rainfall_forecast.pdf
---
+++ Weather/SatelliteImage.sh
@@ -0,0 +1,2 @@
+rm 3Dasiasec_ir1.jpg
+wget https://mausam.imd.gov.in/Satellite/3Dasiasec_ir1.jpg
